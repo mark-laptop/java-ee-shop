b4ad23b4d0aca4d8026d9874f07f7e3b36a7805b
= <=>
2020-11-10 17:09:03 +0300

    update_1

--- servlet-web-app/src/main/java/ru/ndg/controller/CatalogServlet.java
+++ servlet-web-app/src/main/java/ru/ndg/controller/CatalogServlet.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.sql.Connection;
 
 @Log4j2
 @WebServlet(name = "catalogServlet", urlPatterns = {"/catalog", "/catalog/edit", "/catalog/delete", "/catalog/new", "/catalog/update", "/catalog/create"})
